9a3924d606f6251865e37fdc441c9da0b5f92796
poohcz <poohcz@nate.com>
2024-02-19 21:14:49 +0900

    Initial Commit

---
+++ MVC_Todo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC_Todo
+//
+//  Created by 김동율 on 2/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
