3ed8611a7ead225878e77d3daea01a2baca67150
장진호 <wlsgh3786@gmail.com>
2024-02-21 22:16:22 +0900

    - Add 작업중

---
+++ MVC_Todo/Components/CommonLineTextField.swift
@@ -0,0 +1,35 @@
+//
+//  UITextField.swift
+//  MVC_Todo
+//
+//  Created by 장진호 on 2024/02/20.
+//
+
+import UIKit
+
+class CommonLineTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialize()
+    }
+    
+    func initialize() {
+        
+        self.borderStyle = .none
+        let border = CALayer()
+        border.frame = CGRect(x: 0, y: self.frame.size.height-1, width: self.frame.width, height: 1)
+        border.backgroundColor = UIColor.white.cgColor
+        self.layer.addSublayer((border))
+        self.textAlignment = .center
+        self.textColor = UIColor.white
+    }
+    
+    
+    
+}
--- MVC_Todo/VC/InsertVC/InsertVC.swift
+++ MVC_Todo/VC/InsertVC/InsertVC.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import UIKit
+
+class InserVC : UIViewController {
+    
+    @IBOutlet weak var txfTitle: CommonLineTextField!
+    @IBOutlet weak var txfDetail: CommonLineTextField!
+    @IBOutlet weak var btnAdd: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setView()
+    }
+    
+    func setView() {
+        
+    }
+    
+}
